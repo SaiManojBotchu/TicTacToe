0872bac9b03ba6673597689d54dc4f8c4d56b69e
Preetam Varun <preetamarun99@gmail.com>
2021-08-24 21:20:22 +0530

    start project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe/screens/welcome_screen.dart';
+import 'package:tic_tac_toe/screens/pickup_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('TIC-TAC-TOE'),
-          backgroundColor: Colors.green[500],
-        ),
-        body: SafeArea(
-          child: WelcomeScreen(),
-        ),
-      ),
+      home: PickUpScreen(),
     );
   }
+}
