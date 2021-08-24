dc52db119002051d8152f5e9ebad2b88b5b99fb7
Sai Manoj <saimanoj2826@gmail.com>
2021-08-24 20:17:18 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/screens/welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TIC-TAC-TOE'),
+          backgroundColor: Colors.green[500],
+        ),
+        body: SafeArea(
+          child: WelcomeScreen(),
+        ),
+      ),
+    );
   }
-}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
