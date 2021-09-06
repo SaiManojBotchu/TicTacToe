ee27b532e381152efc859359d9dc0eed3114328f
Sai Manoj <saimanoj2826@gmail.com>
2021-09-06 16:32:32 +0530

    changing color variables in constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
-const kBackgroundColor = Color(0xFFFF4B4B);
+const kBackgroundColor = Color(0xFF422BA7);
+const kTextColor = Color(0xFFeae8f2);
 
-const kTextColor = Color(0xFFFFCA26);
+const kXColor = Color(0xFFFF4B4B);
+const kOColor = Color(0xFFFFCA26);
+
+const kContainerColor = Color(0xFF6649C4);
