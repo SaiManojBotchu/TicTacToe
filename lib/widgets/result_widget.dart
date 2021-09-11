2d6f2a83f0cf1420ab27d15522df5912f7f9d5dd
Sai Manoj <saimanoj2826@gmail.com>
2021-09-11 13:18:53 +0530

    1.update to 2 buttons in game screen {play again} and {home}   2.rename lib/widgets/{select_button.dart => gesture_detector_widget.dart}

--- lib/widgets/result_widget.dart
+++ lib/widgets/result_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tic_tac_toe/constants.dart';
+import 'package:tic_tac_toe/screens/game_screen.dart';
 import 'package:tic_tac_toe/widgets/player.dart';
 import 'package:tic_tac_toe/widgets/reusable_button.dart';
 import 'package:delayed_display/delayed_display.dart';
@@ -28,23 +29,31 @@ class ResultWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
-                  player.winner
-                      ? 'assets/images/win.png'
-                      : 'assets/images/draw.png',
+                  Player.winner ? 'assets/images/win.png' : 'assets/images/draw.png',
                   width: MediaQuery.of(context).size.width / 2,
                 ),
                 Text(
-                  player.winner ? player.getWinnerText() : 'Draw',
+                  Player.winner ? player.getWinnerText() : 'Draw',
                   style: kTextStyle,
                 )
               ],
             ),
           ),
-          SizedBox(height: 40.0),
+          SizedBox(height: 30.0),
           ReusableButton(
-            text: 'Restart',
+            text: 'Play Again',
             textSize: 30.0,
-            textPadding: 10.0,
+            textPadding: 8.0,
+            onPressed: () {
+              player.resetData();
+              Navigator.pop(context);
+              Navigator.push(context, MaterialPageRoute(builder: (context) => GameScreen()));
+            },
+          ),
+          ReusableButton(
+            text: 'Home',
+            textSize: 25.0,
+            textPadding: 5.0,
             onPressed: () {
               player.resetData();
               Navigator.pop(context);
