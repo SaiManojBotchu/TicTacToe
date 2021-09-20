6c8000c8dc70d915da2f05d6be178d401866481c
Sai Manoj <saimanoj2826@gmail.com>
2021-09-20 19:57:14 +0530

    bugfix: resume music only when it is playing when opening the app from background

--- lib/models/audio_resume.dart
+++ lib/models/audio_resume.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:tic_tac_toe/utilities/audio_player.dart';
+import 'package:tic_tac_toe/models/settings.dart';
 
 class Handler extends WidgetsBindingObserver {
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      AudioPlayer.playMusic(); // Audio player is a custom class with resume and pause static methods
-    } else {
-      AudioPlayer.pauseMusic();
+    if (Settings.audioValues[1]) {
+      if (state == AppLifecycleState.resumed) {
+        AudioPlayer.playMusic(); // Audio player is a custom class with resume and pause static methods
+      } else {
+        AudioPlayer.pauseMusic();
+      }
     }
   }
 }
